7a36a419e6f1af928fb0318cb34cfae34120fea7
Aaron Dills <aaronsdills@gmail.com>
2022-02-19 17:43:55 -0500

    Delete prepared_queries.php

--- SweetwaterComments/prepared_queries.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$CANDY_QUERY="SELECT * FROM sweetwater_test WHERE comments like '%candy%'";
-$CALL_QUERY="SELECT * FROM sweetwater_test WHERE comments like '%call%'";
-$REFFERED_QUERY="SELECT * FROM sweetwater_test WHERE comments like '%referred%'";
-$SIGNATURE_QUERY="SELECT * FROM sweetwater_test WHERE comments like '%signature%'";
-$MISC_QUERY="SELECT * FROM sweetwater_test WHERE comments not in (SELECT comments FROM sweetwater_test WHERE comments like '%candy%' OR comments like '%call%' OR comments like '%referred%' OR comments like '%signature%')";
