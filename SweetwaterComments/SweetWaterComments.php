b1bcfff9eeb7ee9e9dbf4675f7d1da12f75081ac
Aaron Dills <aaronsdills@gmail.com>
2022-02-19 17:44:11 -0500

    Update SweetWaterComments.php

--- SweetwaterComments/SweetWaterComments.php
+++ SweetwaterComments/SweetWaterComments.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
 require_once './CommentTable.php';
 require_once './Database.php';
 require_once './PreparedStatements.php';
