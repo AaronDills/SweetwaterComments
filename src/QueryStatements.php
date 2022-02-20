a88f3350c07ad50e33769a51e377bd056afd1c73
Aaron Dills <aaronsdills@gmail.com>
2022-02-20 18:04:17 -0500

    Added candy patterns to the not in clause in misc

--- src/QueryStatements.php
+++ src/QueryStatements.php
@@ -10,5 +10,5 @@ class QueryStatements {
     const CALL_QUERY="SELECT * FROM sweetwater_test WHERE (comments like '% call %' OR comments like '% call.%' OR comments like '% calls%' OR comments like '%comunicarse%' OR comments like '%llámame%') AND comments not like '% call your%'";
     const REFFERED_QUERY="SELECT * FROM sweetwater_test WHERE comments like '%referred%'or comments like '%referral%'";
     const SIGNATURE_QUERY="SELECT * FROM sweetwater_test WHERE comments like '%signature%'";
-    const MISC_QUERY="SELECT * FROM sweetwater_test WHERE comments not in (SELECT comments FROM sweetwater_test WHERE comments like '%candy%' OR comments like '% call %' OR comments like '% call.%' OR comments like '% calls%' OR comments like '%comunicarse%' OR comments like '%llámame%' OR comments like '%referred%' or comments like '%referral%' OR comments like '%signature%')";
+    const MISC_QUERY="SELECT * FROM sweetwater_test WHERE comments not in (SELECT comments FROM sweetwater_test WHERE comments like '%candy%' or comments like '%smarties%' or comments like '%taffy%' OR comments like '% call %' OR comments like '% call.%' OR comments like '% calls%' OR comments like '%comunicarse%' OR comments like '%llámame%' OR comments like '%referred%' or comments like '%referral%' OR comments like '%signature%')";
 }
